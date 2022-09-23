7fea26a5bea716b1e8e412eaac5c5c2a6074c0a6
g_garsan@hotmail.com <g_garsan@hotmail.com>
2022-09-24 00:25:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <h2>React + Docker</h2>
+    <p>Docker from 8080 port</p>
+   
+    </div>
+  );
+}
+
+export default App;
